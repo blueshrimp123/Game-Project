4aa5f8ceed75f4a840426895bc38a210e3582b7d
max <61802966+maximilliany@users.noreply.github.com>
2020-03-10 11:02:09 -0700

    changed background color to random, ball color to random, ball size to random

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -9,8 +9,8 @@ function setup() {
 }
 
 function draw(){
-	background(220);
-
+	background(random(255),random(255),random(255));
+  
 //	draw all the balls in that array
 	for (let i = 0; i < balls.length; i++) {
 	    balls[i].drawBall();
@@ -19,28 +19,32 @@ function draw(){
 }
 
 function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(100, 100);
+  let  b = new Ball(random(800), random(400), random(255), random(255), random(255));
   balls.push(b);
-  console.log(balls);
+  print(balls);
 }
 
 //ball class from which to create new balls with similar properties.
 class Ball {
 
-	constructor(x,y){ //every ball needs an x value and a y value
+	constructor(x,y,red, green, blue, diameter){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.diameter = diameter;
 	}
 
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
-    		fill("red");
-		    ellipse(this.x,this.y,10,10);
+    		fill(this.red, this.green, this.blue);
+		    ellipse(this.x,this.y,random(70));
 	}
 
 	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
+		this.x = this.x+ random(2)
+		this.y = this.y+ random(5)
 	}
 
 
